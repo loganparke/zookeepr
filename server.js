2a0d266fb692a7927574fc0812d621d968d20926
Logan Parke <logan.c.parke@gmail.com>
2022-03-28 12:27:54 -0700

    11.1

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -50,6 +55,15 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result){
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
+  })
+
 app.listen(PORT, () => {
-  console.log(`api server now on port 3001`);
+  console.log(`api server now on port ${PORT}`);
 });
